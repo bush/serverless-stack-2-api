c7c3482aaf9a3c9501e7f30b4f22e57a33362644
Dave Bush <davebush@gmail.com>
2018-10-07 11:58:17 -0400

    Fixed mistake with tablenames
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- create.js
+++ create.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "process.env.tableName",
+    TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
@@ -19,6 +19,7 @@ export async function main(event, context, callback) {
     await dynamoDbLib.call("put", params);
     callback(null, success(params.Item));
   } catch (e) {
+    console.log(e);
     callback(null, failure({ status: false }));
   }
 }
